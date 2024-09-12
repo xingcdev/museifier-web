e885ed1d367cf8db466baf07bebcc7d1af4202a7
xingcdev <60323726+xingcdev@users.noreply.github.com>
2024-09-12 22:22:09 +0200

    fix: incorrect logout uri

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,12 +2,12 @@
 
 interface ImportMetaEnv {
 	readonly VITE_OAUTH_AUTHORIZATION_SERVER_URI: string;
+	readonly VITE_OAUTH_TOKEN_URI: string;
+	readonly VITE_OAUTH_LOGOUT_URI: string;
 	readonly VITE_OAUTH_CLIENT_ID: string;
 	readonly VITE_OAUTH_CLIENT_SECRET: string;
 	readonly VITE_OAUTH_REDIRECT_URI: string;
-	readonly VITE_OAUTH_TOKEN_URI: string;
 	readonly VITE_API_URL: string;
-	readonly VITE_OAUTH_LOGOUT_URI: string;
 	readonly VITE_GEOCODING_API_URL: string;
 }
 
