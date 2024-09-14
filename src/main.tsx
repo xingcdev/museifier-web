6b2da9c8bbc8a01ceb5f5a25ea80c7cf03fad106
xingcdev <60323726+xingcdev@users.noreply.github.com>
2024-09-14 17:00:15 +0200

    chore: install keycloakify

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,24 @@ import 'leaflet/dist/leaflet.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app';
+import KcPage from './keycloak-theme/login/KcPage';
 import './styles/main.css';
+
+// The following block can be uncommented to test a specific page with `yarn dev`
+// Don't forget to comment back or your bundle size will increase
+/*
+import { getKcContextMock } from "./login/KcPageStory";
+
+if (import.meta.env.DEV) {
+    window.kcContext = getKcContextMock({
+        pageId: "register.ftl",
+        overrides: {}
+    });
+}
+*/
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		{window.kcContext ? <KcPage kcContext={window.kcContext} /> : <App />}
 	</StrictMode>
 );
