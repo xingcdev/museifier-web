983f2e65361c36db27967eaa067805321de045fc
xingcdev <60323726+xingcdev@users.noreply.github.com>
2024-09-21 22:27:48 +0200

    feat: scroll restoration when navigating

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, ScrollRestoration } from 'react-router-dom';
 import { AppHeader } from '../components/app-header';
 import { RequireAuth } from '../components/require-auth';
 import { AppFooter } from '../components/ui/app-footer';
@@ -6,6 +6,7 @@ import { AppFooter } from '../components/ui/app-footer';
 export default function Layout() {
 	return (
 		<RequireAuth>
+			<ScrollRestoration />
 			<AppHeader />
 			<Outlet />
 			<AppFooter />
